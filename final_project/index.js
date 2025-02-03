8316672e313743e2bf85cb3dcf45b40b210ba493
Declan Jones <jandaservicesandventures@gmail.com>
2025-02-02 22:06:22 -0500

    First Commit

--- final_project/index.js
+++ final_project/index.js
@@ -11,8 +11,23 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+    const token = req.headers.authorization?.split(' ')[1] || req.cookies?.jwt
+
+    if (!token) {
+        return res.status(401).json({ message: 'No token Provided'});
+    }
+
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if (err) {
+            console.error("JWT verification error:", err);
+            return res.status(401).json({ message: 'Invalid token'});
+        }
+
+        // Token is valid. 'decoded contains the user info payload.
+        req.customer = decoded;
+
+        next()
+    })
  
 const PORT =5000;
 
